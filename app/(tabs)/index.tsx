bf6c30dbb2753cda607e3d8e48ffe49f72cc414f
Furkan Aslan <furkanaslan1209@gmail.com>
2025-03-11 17:56:09 +0300

    finish 8  understanding webhooks

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
-import { View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "../../styles/auth.styles";
-import { Link } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function Index() {
+  const { signOut } = useAuth();
   return (
     <View style={styles.container}>
-      <Link href={"/notifications"}>Feed screen in tabs</Link>
+      <TouchableOpacity onPress={() => signOut()}>
+        <Text style={{ color: "white" }}>SignOut</Text>
+      </TouchableOpacity>
     </View>
   );
 }
